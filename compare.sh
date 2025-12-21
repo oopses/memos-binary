f1ce41917e0eaa876b470cf9bb56407276ee0849
Link <oops@codechat.top>
2025-12-21 23:40:53 +0800

    Enhance version comparison logic in compare.sh

--- compare.sh
+++ compare.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-if [[ "$LOCALVERSION" == "$REMOTEVERSION" ]]; then
-    echo "SYNCED=1" >> $GITHUB_ENV
+echo "Remote version: $REMOTEVERSION"
+echo "Local version:  $LOCALVERSION"
+
+if [ -z "$LOCALVERSION" ]; then
+  echo "LOCALVERSION is empty, treating as not synced."
+  echo "SYNCED=0" >> $GITHUB_ENV
+  exit 0
+fi
+
+if [ "$REMOTEVERSION" = "$LOCALVERSION" ]; then
+  echo "Versions are synced."
+  echo "SYNCED=1" >> $GITHUB_ENV
 else
-    echo "SYNCED=0" >> $GITHUB_ENV
+  echo "Versions differ. Need to trigger build."
+  echo "SYNCED=0" >> $GITHUB_ENV
 fi
